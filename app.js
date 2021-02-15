2c61ef136be90323d3e13c9cf5a8a48115f1416c
Sean <seanom96@gmail.com>
2021-02-15 17:20:22 +0000

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const query = require("./businessCategories");
+let fs = require("fs");
+let count = 0;
+
+function checkDomains() {
+  axios
+    .post("https://domains.google.com/v1/Main/FeSearchService/Search", {
+      clientFilters: {},
+      clientUserSpec: {
+        countryCode: "US",
+        currencyCode: "USD",
+        sessionId: "-934712866",
+      },
+      debugType: "DEBUG_TYPE_NONE",
+      query: query[count],
+    })
+    .then((res) => {
+      let response = JSON.parse(res.data.slice(6));
+      console.log(response.searchResponse.results.result);
+      let availableDotComDomains = [""];
+      if (response.searchResponse.results.result[0].supportedResultInfo) {
+        availableDotComDomains = response.searchResponse.results.result
+          .filter(
+            (item) =>
+              item.domainName.tld == "com" &&
+              item.supportedResultInfo.availabilityInfo.availability ==
+                "AVAILABILITY_AVAILABLE"
+          )
+          .map(
+            (domain) =>
+              `${query[count]}\t${domain.domainName.sld}.${domain.domainName.tld}\n`
+          );
+      }
+      console.log(availableDotComDomains);
+      fs.appendFile("my_list.txt", availableDotComDomains.join(""), (err) => {
+        if (err) throw err;
+        if (count < query.length - 1) {
+          count++;
+          setTimeout(checkDomains, 1000);
+        } else {
+          console.log("done");
+        }
+      });
+    });
+}
+checkDomains();
